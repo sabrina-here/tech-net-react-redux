c9b8a390afc7572033f214423ff2cb12da1fe129
sabrina-here <iknowthissabrina@gmail.com>
2024-01-21 09:06:38 +0600

    m

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,6 +3,7 @@ import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
 import { Switch } from '@/components/ui/switch';
 import { useToast } from '@/components/ui/use-toast';
+import { useGetProductsQuery } from '@/redux/api/apiSlice';
 import {
   setPriceRange,
   toggleState,
@@ -18,6 +19,8 @@ export default function Products() {
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
+
+  // const { data, isLoading } = useGetProductsQuery(undefined);
 
   const { priceRange, status } = useAppSelector((state) => state.product);
   const dispatch = useAppDispatch();
